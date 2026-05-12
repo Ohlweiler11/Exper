4a9578237dfe542690bf39d06c00aa4d2be065b8
Ohlweiler11 <henriquecostaohlweiler@gmail.com>
2026-05-12 20:19:59 -0300

    Fixes tokenizer

--- modules/tokenizer.py
+++ modules/tokenizer.py
@@ -1,6 +1,6 @@
 def get_main_tokens(line: str) -> list[str]:
     tokens = line.split()
-    return [token for token in tokens if token[:2] == "--"]
+    return [token for token in tokens if token[:2] != "--"]
 
 def get_options(line: str) -> dict[str, str]:
     tokens = line.split()
